64e1051e18b8b3a829a37ae8076ac1d9ee39eb36
Felipe Sartorato <77643258+FelipeSartuu@users.noreply.github.com>
2024-04-22 15:19:18 -0300

    base do projeto  c#
    classe contato

---
+++ ListaDeContatosT7/contato.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatosT7
+{
+    internal class contato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        //Propriedades (get e set) é um controle de acesso às variáveis
+
+        public string Nome {  get { return nome; } set {  nome = value; } }
+
+        public string Sobrenome { get {  return sobrenome; } set {  sobrenome = value; } }
+
+        public string Telefone { get {  return telefone; } 
+            set {
+                if (value.Length == 11)
+                {
+                    telefone = value;
+                }
+
+                else
+                {
+                    telefone = "00-00000-0000";
+                }
+            } 
+        }
+
+        //O método que tem o mesmo nome da classe e não retorna nada é chamado de construtor
+        public contato()
+        {
+            Nome = "José";
+            Sobrenome = "Da Silva";
+            Telefone = "11-99999-9999";
+        }
+
+        public contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+        //Sobreescreve o método ToString()
+        public override string ToString()
+        {
+            string saida = string.Empty;
+            saida += String.Format("{0}, {1}", Nome, Sobrenome);
+            saida += String.Format("({0}) {1}-{2}", Telefone.Substring(0, 1), Telefone.Substring(2, 5), Telefone.Substring(8, 4));
+            return saida;
+        }
+    }
+}
